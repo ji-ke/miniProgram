5bb3ba49be828c3473dc5216ba1a28303af61848
lisa <1915716933@qq.com>
2020-06-25 22:06:39 +0800

    初始化项目

---
+++ pages/bufan/bufan.js
@@ -0,0 +1,89 @@
+// pages/bufan/bufan.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    content: '不凡学院老师由一帮80后老北漂组成。我们分别来自于腾讯网，凤凰网，网易 等北京知名互联网公司，我们都曾奋斗在一线岗位，都曾面试新人，带新人，深知公司需要什么样的人才。我们会通过实战项目结合 培训的教学模式，把知名互联网公司的工作流程，设计方法，开发方 式手把手的教给大家，在不凡学习就相当于工作，高薪就业你也行！',
+    list: [
+      {
+        title: 'UI设计',
+        content: '就业方向：网页设计，APP设计，管理系统软件设计 智能电视，车载系统，智能穿戴设备等软件设计',
+        button: '课程详情',
+        class: 'ui'
+      },
+      {
+        title: 'H5前端开发',
+        content: '就业方向：网站开发，网页特效开发，APP开发 微信小程序开发，网页游戏开发，移动WEB开发',
+        button: '课程详情',
+        class:"web"
+      }
+
+    ],
+    img: [
+      {
+        imgSrc: '/assets/images/bufan/evn_1.jpg'
+      },
+      {
+        imgSrc: '/assets/images/bufan/evn_4.jpg'
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
